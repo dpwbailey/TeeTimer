a3a8df11bb197c2850a3105cc98e1cefbbe26b31
dpwba <dpwbailey@gmail.com>
2019-11-06 11:39:16 -0500

    newcommit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+
+    @FXML
+    private Button generateScheduleButton;
+
+    @FXML
+    void generateSchedule(MouseEvent event) throws IOException {
+
+        Stage thisStage = (Stage) generateScheduleButton.getScene().getWindow();
+        Parent scheduleScreen = FXMLLoader.load(getClass().getResource("displaySchedule.fxml"));
+        thisStage.setScene(new Scene(scheduleScreen, 750, 500));
+    }
+
 }
